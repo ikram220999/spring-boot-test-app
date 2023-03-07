75bdcc851b4a4910e6094b679adc501ada01506d
Muhammad Ikram bin Azhar <85838927+ikram220999@users.noreply.github.com>
2023-03-07 17:44:32 +0800

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(DemoApplication.class, args);
+    }
+
+	@GetMapping("/kambing")
+    public String kambing(@RequestParam(value = "name2", defaultValue = "kambing") String name) {
+      return String.format("Helloasdas %s!", name);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+
+	@GetMapping("/testing")
+    public String testing(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
